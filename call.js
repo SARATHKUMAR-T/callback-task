b9f956ac03292ee65b39c1f4f39e1fec160aea1d
SARATHKUMAR-T <123524854+SARATHKUMAR-T@users.noreply.github.com>
2023-03-22 09:58:09 +0530

    Add files via upload

---
+++ call.js
@@ -0,0 +1,41 @@
+var s1=document.createElement("div");
+s1.setAttribute("id","main");
+document.body.append(s1);
+
+
+
+setTimeout(()=>{
+    document.getElementById("main").innerHTML=10;
+    setTimeout(()=>{
+        document.getElementById("main").innerHTML=9;
+        setTimeout(()=>{
+            document.getElementById("main").innerHTML=8;
+            setTimeout(()=>{
+                document.getElementById("main").innerHTML=7;  
+                setTimeout(()=>{
+                    document.getElementById("main").innerHTML=6; 
+                    setTimeout(()=>{
+                        document.getElementById("main").innerHTML=5;  
+                        setTimeout(()=>{
+                            document.getElementById("main").innerHTML=4; 
+                            setTimeout(()=>{
+                                document.getElementById("main").innerHTML=3;
+                                setTimeout(()=>{
+                                    document.getElementById("main").innerHTML=2; 
+                                    setTimeout(()=>{
+                                        document.getElementById("main").innerHTML=1;  
+                                        setTimeout(()=>{
+                                            document.getElementById("main").innerHTML=`HAPPY <br>INDEPENDANCE DAY`;  
+                                        },1000)
+                                    },1000) 
+                                },1000)  
+                            },1000) 
+                        },1000)
+                    },1000) 
+                },1000)
+            },1000)  
+        },1000)  
+    },1000)
+
+
+},1000)
